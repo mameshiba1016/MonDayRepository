1f65f594ddc061791ed4222b4f75eeff68ba1579
gityone <aotuki2114006kaede@gmail.com>
2026-07-06 09:52:37 +0900

    wa-i

---
+++ 20260706_GoharaMahiro/20260706.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void Add(int num)
+{
+	num += 10;
+}
+
+int main(void)
+{
+	int number;
+
+	std::cout << "”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << std::endl;
+
+	std::cin >> number;
+
+	Add(number);
+
+	std::cout << number << "‚Å‚·" << std::endl;
+
+	return 0;
+
+}
